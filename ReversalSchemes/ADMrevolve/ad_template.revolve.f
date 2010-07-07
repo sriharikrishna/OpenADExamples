9c02df546cd29264652f5642de301d9fdbe0891b
utke <utke@linux-l3b2.site>
2010-07-07 03:20:56 -0500

    fix up template to account for checkpoints used in outer joint mode

--- ReversalSchemes/ADMrevolve/ad_template.revolve.f
+++ ReversalSchemes/ADMrevolve/ad_template.revolve.f
@@ -8,7 +8,7 @@
           LOGICAL :: initialized=.FALSE.
           TYPE(rvAction) :: theAction
           CHARACTER , DIMENSION(80) :: errorMsg
-          integer :: cpCount
+          integer :: jointCPCount
           integer :: currIter
           type(modeType) :: our_orig_mode
 
@@ -26,34 +26,26 @@
             revStatsFCounters%__SRNAME__=revStatsFCounters%__SRNAME__+1
             our_orig_mode=our_rev_mode
             our_rev_mode%arg_store=.FALSE.
-!$PLACEHOLDER_PRAGMA$ id=1
+            DO ILOOP = 1, N_MAX, 1
+              CALL box_forward(ILOOP)
+            END DO
             our_rev_mode=our_orig_mode
           end if 
           if (our_rev_mode%tape) then
-            revStatsFCounters%__SRNAME__=revStatsFCounters%__SRNAME__+1
-            call OAD_revStorePlain
-C            print*, " tape       ", our_rev_mode
-!$PLACEHOLDER_PRAGMA$ id=2
-            call OAD_revAdjoint
-          end if 
-          call revStatsUpdateTape(oad_dt_ptr-1)
-          if (our_rev_mode%adjoint) then
-             our_orig_mode=our_rev_mode
              currIter=0
-             write (*,fmt='(a)',advance='no') 'number of checkpoints = '
-             read (*,*) cpCount 
-             CALL rvVerbose(3)
-             initialized=rvInit(n_max,cpCount,errorMsg)
+             jointCPcount=cp_fNumber()
+!             CALL rvVerbose(3)
+             initialized=rvInit(n_max,revStatsRevolveCPcount,errorMsg)
              IF (.NOT.initialized) WRITE(*,'(A,A)') 'Error: ', errorMsg
              do while (theAction%actionFlag/=rvDone)
                 theAction=rvNextAction()
                 select case (theAction%actionFlag) 
                 case (rvStore)
-                  call cp_write_open(theAction%cpNum)
+                  call cp_write_open(theAction%cpNum+jointCPCount)
 !$PLACEHOLDER_PRAGMA$ id=4
                   call cp_close
                 case (rvRestore)
-                  call cp_read_open(theAction%cpNum)
+                  call cp_read_open(theAction%cpNum+jointCPCount)
 !$PLACEHOLDER_PRAGMA$ id=6
                   currIter=theAction%iteration
                   call cp_close
@@ -69,7 +61,7 @@ C            print*, " tape       ", our_rev_mode
                   call box_forward(currIter)
                 end select   
               end do
-              our_rev_mode=our_orig_mode
+              call OAD_revRestoreTape
             end if 
           call revStatsUpdateCP(cp_fNumber()-1)
        end subroutine template
