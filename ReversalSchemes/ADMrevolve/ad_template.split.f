2f25ae4124a5a086a14722fbcd396e3e8bc12bed
utke <utke@linux-l3b2.site>
2010-07-08 00:29:38 -0500

    need to update when the lower level routines run in split mode invoked by revolve

--- ReversalSchemes/ADMrevolve/ad_template.split.f
+++ ReversalSchemes/ADMrevolve/ad_template.split.f
@@ -12,6 +12,7 @@
           if (our_rev_mode%tape) then
             revStatsFCounters%__SRNAME__=revStatsFCounters%__SRNAME__+1
 !$PLACEHOLDER_PRAGMA$ id=2
+            call revStatsUpdateTape(oad_dt_ptr-1)
           end if 
           if (our_rev_mode%adjoint) then
 !$PLACEHOLDER_PRAGMA$ id=3
