d2b8e095fc33f04b371d69789c01cecbfbbd84b3
utke on macbook <utke on macbook@localhost>
2011-06-13 21:25:19 -0500

    to be sure save A

--- LibWrappers/UmfPack_2.2_passive/ADMsplit/Templates/solve.f90
+++ LibWrappers/UmfPack_2.2_passive/ADMsplit/Templates/solve.f90
@@ -12,9 +12,16 @@ subroutine template()
      passiveB=b%v
      call solve(n,A,passiveX,passiveB,trans)
      x%v=passiveX
+     ! save A (do by hand what the inliner does)
+     double_tape(double_tape_pointer:double_tape_pointer+ &
+          (size(A,1)*size(A,2))-1)=reshape(A,(/size(A,1)*size(A,2)/))
+     double_tape_pointer=double_tape_pointer+(size(A,1)*size(A,2))
   end if
   if (our_rev_mode%adjoint) then
      passiveX=x%d
+     ! restore A: (do by hand what the inliner does)
+     double_tape_pointer=double_tape_pointer-(size(A,1)*size(A,2))
+     A(:,:)=reshape(double_tape(double_tape_pointer:),shape(A))
      call solve(n,A,passiveB,passiveX, .not. trans)  ! <---- flipping the transpose flag 
      b%d=b%d+passiveB
      x%d=0.0
